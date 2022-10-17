7924f751d1e3391671774e25772f4373c74bcae9
GitHubYanis <yanis21p@gmail.com>
2022-10-17 12:21:49 -0400

    update

--- exercice.py
+++ exercice.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+import math
 
 
 # TODO: Définissez vos fonction ici
-
+def vol_et_masse(x=1,y=2,z=3,masse_volumique=4):
+    volume = 4/3 * x * y * z * math.pi
+    masse = masse_volumique * volume
+    return volume, masse
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
+    print(vol_et_masse())
 
     pass
